2f4302505a08830307a571ca5e34a500f8e3e0a0
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-18 18:51:12 +0530

    new file:   .eslintrc.cjs 	new file:   .gitignore 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.jsx 	new file:   src/components/Navbar/Navbar.css 	new file:   src/components/Navbar/Navbar.jsx 	new file:   src/components/home/Home.css 	new file:   src/components/home/Home.jsx 	new file:   src/fonts/ASIX-FOUNDER-Italic.otf 	new file:   src/fonts/ASIX-FOUNDER.otf 	new file:   src/fonts/Giga.otf 	new file:   src/fonts/Kilo.otf 	new file:   src/fonts/MagnoSans-Black.woff2 	new file:   src/fonts/MagnoSans-Bold.ttf 	new file:   src/fonts/Medium.otf 	new file:   src/fonts/Roadam.otf 	new file:   src/main.css 	new file:   src/main.jsx 	new file:   vite.config.js

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/home/Home";
+
+const App = () => {
+  return (
+    <>   
+      <Navbar />
+      <Home />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <>
+      <div className="main__navbar">
+        <div className="navbar__heading">
+          <h1>PRATHAMESH</h1>
+        </div>
+        <div className="Navbar__content"></div>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
