a0956d1365c35ca544f22a2fe1f49962fb592354
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-25 17:44:47 +0530

    modified:   src/App.jsx 	modified:   src/components/Navbar/Navbar.css 	new file:   src/components/mousefollower/Mouse.css 	new file:   src/components/mousefollower/Mouse.jsx 	modified:   src/main.css

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@ import Home from "./components/home/Home";
 import Project from "./components/project/Project";
 import Skillsection from "./components/skillsection/SkillSection";
 import "./app.css";
+import Mouse from "./components/mousefollower/Mouse";
 const App = () => {
   return (
     <>
+    <Mouse/>
       <Skillsection />
       <Navbar />
       <Home />
---
+++ src/components/mousefollower/Mouse.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+import "./Mouse.css";
+import { motion } from "framer-motion";
+
+const Mouse = () => {
+  const [position, setposition] = useState({
+    x: null,
+    y: null,
+  });
+
+  useEffect(() => {
+    window.addEventListener("mousemove", (e) => {
+      setposition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    });
+  }, []);
+
+  return (
+    <motion.div
+      animate={{
+        x: position.x ,
+        y: position.y ,
+      }}
+      
+      className="cursor"
+    ></motion.div>
+  );
+};
+
+export default Mouse;
